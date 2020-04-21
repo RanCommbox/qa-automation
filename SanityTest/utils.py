d723a3069a6fa782519693638297db96c3126a86
ranBumpYard <ran.ya@bumpyard.com>
2020-04-21 18:46:53 +0300

    headless

--- SanityTest/utils.py
+++ SanityTest/utils.py
@@ -1,12 +1,12 @@
 import pytest
-from config import Config
-from helper_base import HelperBase
-from test_name import TestNames
+from SanityTest.config import Config
+from SanityTest.helper_base import HelperBase
+from SanityTest.test_name import TestNames
 
 
 @pytest.yield_fixture
 def before_after_test_actions(is_headless):
-    HelperBase.get_driver_and_configure_browser_and_url(Config.SERVER_URL, is_headless, False)
+    HelperBase.get_driver_and_configure_browser_and_url(Config.SERVER_URL, is_headless, True)
     HelperBase.login_to_commbox(Config.ADMIN_USER, Config.ADMIN_PASSWORD, Config.ADMIN_BRAND)
     yield
     if is_headless is not 'True':

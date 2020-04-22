877f39cc22267307177afd46765a7b7736756298
Ofer Shalev <ofer.s@commbox.io>
2020-04-22 10:42:22 +0300

    enable test

--- SanityTest/test_suite.py
+++ SanityTest/test_suite.py
@@ -10,7 +10,6 @@ from SanityTest.api_sanity_test import ApiTesting
 #suite = unittest.TestLoader().loadTestsFromTestCase(test_get_state()
 #unittest.TextTestRunner(verbosity=2).run(suite)
 
-
 first_test = TestLoader().loadTestsFromTestCase(ApiTesting)
 suite = TestSuite(first_test)
 runner = TextTestRunner()

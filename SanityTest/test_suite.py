bd48818d6ed63ffed35573a1c654d598f9eac458
Ofer Shalev <ofer.s@commbox.io>
2020-04-22 10:39:45 +0300

    disable login test for debug

--- SanityTest/test_suite.py
+++ SanityTest/test_suite.py
@@ -4,7 +4,7 @@ import sys, os
 myPath = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, myPath + '/../')
 import SanityTest.api_sanity_test
-from SanityTest.login_test import loginfirsttest
+#from SanityTest.login_test import loginfirsttest
 from SanityTest.api_sanity_test import ApiTesting
 
 #suite = unittest.TestLoader().loadTestsFromTestCase(test_get_state()
